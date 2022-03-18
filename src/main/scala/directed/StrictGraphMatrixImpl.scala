9029b8d2dcff0616a4ba662812f7e000f89c8f61
Kieffer Florent <kiefferflo@eisti.eu>
2022-03-18 14:46:11 +0100

    StrictGraphMatrixImpl

--- src/main/scala/directed/StrictGraphMatrixImpl.scala
+++ src/main/scala/directed/StrictGraphMatrixImpl.scala
@@ -8,30 +8,62 @@ package directed
 case class StrictGraphMatrixImpl[V](vs : Seq[V], adjacency : IndexedSeq[IndexedSeq[Int]]) extends StrictGraph[V] {
 
     /** @inheritdoc */
-    lazy val vertices : Set[V] = ???
+    lazy val vertices : Set[V] = vs.toSet
 
     /** @inheritdoc */
-    lazy val arcs : Set[Arc[V]] = ???
+    lazy val arcs : Set[Arc[V]] = (vs foldLeft Set.empty[Arc[V]])
+    {
+        (s,v1) => (vs foldLeft s)
+        {
+            (s,v2) => if (adjacency(vs.indexOf(v1))(vs.indexOf(v2))==1) s + Arc(v1,v2) else s
+        }
+    }
 
     /** @inheritdoc */
-    def successorsOf(v : V) : Option[Set[V]] = ???
+    def successorsOf(v : V) : Option[Set[V]] = (arcs foldLeft Set.empty[V]) { (s, a) => if (a._1 == v) s + a._2 else s }
+    match {
+        case x if x.nonEmpty => Some(x)
+        case _ => None
+    }
 
     /** @inheritdoc */
-    def + (v : V) : StrictGraphMatrixImpl[V] = ???
+    def + (v : V) : StrictGraphMatrixImpl[V] = StrictGraphMatrixImpl(vs :+ v, adjacency)
 
     /** @inheritdoc */
-    def - (v : V) : StrictGraphMatrixImpl[V] = ???
+    def - (v : V) : StrictGraphMatrixImpl[V] = StrictGraphMatrixImpl(vs filterNot { _ == v },
+        (adjacency splitAt(vs indexOf v) match {
+            case(x,y)=> x :++ (y drop 1)
+        })
+          map { _ splitAt(vs indexOf v) match {
+            case(x,y)=> x :++ (y drop 1)
+        }}
+    )
 
     /** @inheritdoc */
-    def +| (e : Arc[V]) : StrictGraphMatrixImpl[V] = ???
+    def +| (e : Arc[V]) : StrictGraphMatrixImpl[V] = StrictGraphMatrixImpl(vs,(adjacency.zipWithIndex foldLeft IndexedSeq.empty[IndexedSeq[Int]]) {
+        (adj,a)=> adj :+
+          (if (vs(a._2)==e._1)
+              (a._1.zipWithIndex foldLeft IndexedSeq.empty[Int]) {
+                  (adjt,i) =>adjt :+  (if (vs(i._2)==e._2) 1 else i._1)
+              }
+          else a._1)
+    })
 
     /** @inheritdoc */
-    def -| (e : Arc[V]) : StrictGraphMatrixImpl[V] = ???
+    def -| (e : Arc[V]) : StrictGraphMatrixImpl[V] = StrictGraphMatrixImpl(vs,(adjacency.zipWithIndex foldLeft IndexedSeq.empty[IndexedSeq[Int]]) {
+        (adj,a)=> adj :+
+          (if (vs(a._2)==e._1)
+              (a._1.zipWithIndex foldLeft IndexedSeq.empty[Int]) {
+                  (adjt,i) =>adjt :+  (if (vs(i._2)==e._2) 0 else i._1)
+              }
+          else a._1)
+    })
 
     /** @inheritdoc */
-    def withoutArc : StrictGraphMatrixImpl[V] = ???
+    def withoutArc : StrictGraphMatrixImpl[V] = StrictGraphMatrixImpl(vs, adjacency.empty)
 
     /** @inheritdoc */
-    def withAllArcs : StrictGraphMatrixImpl[V] = ???
+    def withAllArcs : StrictGraphMatrixImpl[V] = StrictGraphMatrixImpl(vs,
+        (for (x<-vs) yield(for(x<-vs) yield 1).toIndexedSeq).toIndexedSeq)
 }
 
