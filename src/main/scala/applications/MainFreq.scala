9795a7432fd289c4171ecacace6d31634a75cdf0
Kieffer Florent <kiefferflo@eisti.eu>
2022-04-03 16:43:58 +0200

    Ajout de ressources de test

--- src/main/scala/applications/MainFreq.scala
+++ src/main/scala/applications/MainFreq.scala
@@ -4,7 +4,7 @@ object MainFreq {
   def main(args: Array[String]): Unit = {
     val nonInter = args(0).toDouble
     val INSEE = args(1)
-    val graph = FileReading("./src/main/resources/antennes.csv").AntenneProcessor(nonInter,INSEE)
+    val graph = FileReading("./src/main/resources/testA.csv").AntenneProcessor(nonInter,INSEE)
     println(graph.greedyColoring) //TODO meilleur affichage (dot string ?) c'est compliqué (le graph est trop grand)
   }
 }
